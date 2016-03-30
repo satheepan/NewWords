6f08ae46f908d45e6974a84f7e7c74267e406961
Beven Elankumaran <Beven@dyn-118-139-76-246.its.monash.edu.au>
2016-03-30 15:21:34 +1100

    add cool stuff

--- client/index.js
+++ client/index.js
@@ -1,7 +1,37 @@
 var myApp = angular.module('myApp',[])
 
-myApp.controller('MyController', function ($scope) {
-	$scope.newWord = 'cat'
+myApp.service('HistoryService', function ($http){
+	var baseUrl = "http://localhost:8080/"
+
+	//<!--string has the address of our server -->
+
+	this.saveWord = function (newWord) {
+		var url = baseUrl + "saveCurrent"
+		
+		return $http.post(url, {"word": newWord})
+		
+	}
 })
+
+myApp.controller('MyController', function ($scope, HistoryService) {
+	$scope.newWord = 'cat'
+
+	$scope.saveThisWord= function(){
+		HistoryService.saveWord( $scope.newWord)
+		.then(saveSuccess, error)
+	}
+	function saveSuccess (json) {
+		console.log(json)
+	}
+	function error (err) {
+		console.log(err)
+	}
+})
+
+/*myapp controller controls the interface part
+scope is interfacing with the html part 
+scope is the html file*/
+
+
 
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,30 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+var Nedb = require('nedb')
+
+var database = new Nedb({filename: './data/data.db', autoload:true})
 
 app.use(express.static('../client'))
+
+app.use(bodyParser.json())
+
+app.post('/saveCurrent', function (req, res) {
+	var data = {
+				word: req.body.word, 
+				date: Date.now()
+			}
+	//creating a variable called data 
+
+	var done = function (){
+		console.log('I just wrote to the database')
+		res.end("done")
+	}
+
+	database.insert(data, done) //insert the data and run the done function 
+})
+
+//<!--request is, response is what we are going to send back-->
 
 var PORT = 8080
 app.listen(PORT)
